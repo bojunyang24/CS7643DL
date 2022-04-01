1479108e7fb67b2a3b3d9119d135ba5ea00f6756
Bojun Yang <bojunyang24@gmail.com>
2022-04-01 14:43:56 -0400

    hw1

---
+++ HW1/Code/student_version/models/softmax_regression.py
@@ -0,0 +1,84 @@
+# Do not use packages that are not in standard distribution of python
+import numpy as np
+
+from ._base_network import _baseNetwork
+
+class SoftmaxRegression(_baseNetwork):
+    def __init__(self, input_size=28*28, num_classes=10):
+        '''
+        A single layer softmax regression. The network is composed by:
+        a linear layer without bias => ReLU activation => Softmax
+        :param input_size: the input dimension
+        :param num_classes: the number of classes in total
+        '''
+        super().__init__(input_size, num_classes)
+        self._weight_init()
+
+    def _weight_init(self):
+        '''
+        initialize weights of the single layer regression network. No bias term included.
+        :return: None; self.weights is filled based on method
+        - W1: The weight matrix of the linear layer of shape (num_features, hidden_size)
+        '''
+        np.random.seed(1024)
+        self.weights['W1'] = 0.001 * np.random.randn(self.input_size, self.num_classes)
+        self.gradients['W1'] = np.zeros((self.input_size, self.num_classes))
+
+    def forward(self, X, y, mode='train'):
+        '''
+        Compute loss and gradients using softmax with vectorization.
+
+        :param X: a batch of image (N, 28x28)
+        :param y: labels of images in the batch (N,)
+        :return:
+            loss: the loss associated with the batch
+            accuracy: the accuracy of the batch
+        '''
+        loss = None
+        gradient = None
+        accuracy = None
+        #############################################################################
+        # TODO:                                                                     #
+        #    1) Implement the forward process and compute the Cross-Entropy loss    #
+        # Hint:                                                                     #
+        #   Store your intermediate outputs before ReLU for backwards               #
+        #############################################################################
+        z = np.dot(X,self.weights['W1'])
+        r = self.ReLU(z)
+        s = self.softmax(r)
+        loss = self.cross_entropy_loss(s, y)
+        accuracy = self.compute_accuracy(s, y)
+        #############################################################################
+        #                              END OF YOUR CODE                             #
+        #############################################################################
+        if mode != 'train':
+            return loss, accuracy
+
+        #############################################################################
+        # TODO:                                                                     #
+        #    1) Implement the backward process:                                     #
+        #        1) Compute gradients of each weight and bias by chain rule         #
+        #        2) Store the gradients in self.gradients                           #
+        #############################################################################
+        # DL/DW = DL/DR * DR/DZ * DZ/DW
+        num_examples = y.shape[0]
+        s[range(num_examples),y] -= 1
+        dl_dr = s/num_examples
+        dr_dz = self.ReLU_dev(z)
+        dl_dz = dl_dr * dr_dz
+        # dz_dw = X
+        dl_dw = np.dot(X.T, dl_dz)
+        self.gradients['W1'] = dl_dw
+
+        #############################################################################
+        #                              END OF YOUR CODE                             #
+        #############################################################################
+        return loss, accuracy
+
+
+
+
+
+        
+
+
